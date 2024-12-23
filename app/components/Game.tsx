20db7df85c853b398fc2bdcea53d1d3626102ee6
thadar <thadarshinhtet2019@gmail.com>
2024-12-23 12:49:19 +0630

    all functions are added in Phaser

--- app/components/Game.tsx
+++ app/components/Game.tsx
@@ -1,6 +1,5 @@
 'use client'
 import React, { useState } from 'react'
-import FillWords from './FillWords'
 import dynamic from 'next/dynamic'
 
 const PhaserGame = dynamic(() => import('./GameComponent'), { ssr: false });
@@ -10,8 +9,7 @@ const PhaserGame = dynamic(() => import('./GameComponent'), { ssr: false });
 const Game = () => {
   return (
     <div className='flex flex-row bg-blue-300'>
-        {/* <PhaserGame /> */}
-        <FillWords/>
+        <PhaserGame />
     </div>
   )
 }
--- app/components/GameComponent.tsx
+++ app/components/GameComponent.tsx
@@ -2,59 +2,23 @@
 
 import { useEffect, useRef } from 'react';
 import Phaser from 'phaser';
-import {  useMyContext } from '../context/MyProvider';
+import FillWordScene from './FillWordsScene';
 
 
 
 const GameComponent = () => {
   const gameRef = useRef<Phaser.Game | null>(null);
-  const {isDown}=useMyContext()
 
-  // Phaser Scene for Fall
-  class FallScene extends Phaser.Scene {
-    rope: Phaser.Physics.Arcade.Image | null = null;
-    cursor: Phaser.Types.Input.Keyboard.CursorKeys | null = null;
-    isDown: boolean = false;
 
-    constructor() {
-      super({ key: 'fall-scene' });
-    }
-
-    preload() {
-      this.load.image('rope', '/assets/hotairballon.jpg');
-    }
-
-    create() {
-      this.rope = this.physics.add.image(1000,200, 'rope');
-      this.rope.setScale(0.3,0.3)
-
-      if (this.input.keyboard) {
-        this.cursor = this.input.keyboard.createCursorKeys();
-      }
-
-      // Access isDown directly from scene data
-      this.isDown = isDown;
-
-      console.log('isDown:', this.isDown);
-    }
-
-    update() {
-      if (this.isDown && this.rope) {
-          this.rope.y=this.rope.y+10
-      }
-    }
-  }
 
   useEffect(() => {
-    if (gameRef.current) {
-      return; // Prevent reinitialization
-    }
+    if (gameRef.current) return;
 
     const gameConfig: Phaser.Types.Core.GameConfig = {
       type: Phaser.AUTO,
       width: 1400,
       height: 700,
-      scene: [FallScene],
+      scene: [FillWordScene],
       physics: {
         default: 'arcade',
         arcade: {
@@ -74,12 +38,13 @@ const GameComponent = () => {
         gameRef.current = null;
       }
     };
-  }, [isDown]);
+  }, []);
 
-  return (
-   
-      <div id="phaser-container" ></div>
-  );
+
+
+  return <div id="phaser-container"></div>;
 };
 
 export default GameComponent;
+
+
